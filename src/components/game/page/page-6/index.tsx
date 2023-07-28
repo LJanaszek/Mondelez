f006c071b59753b03050eaac6a3f9abe3eb8fb70
skirk20 <lukasjanaszek@gmail.com>
2023-07-28 14:20:10 +0200

    warunek ukonczenia lista

--- src/components/game/page/page-6/index.tsx
+++ src/components/game/page/page-6/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import Box from "../../../layout/box/box"
 import PageLayout from "../../../page-layout"
 import Page92Widget2 from "./widget-2"
@@ -7,23 +8,25 @@ type Props = {
 }
 
 export default function Page6({onNext}: Props) {
+    const [showNext, setShowNext] = useState(false);
     return <Box>
         <PageLayout>
         <p>Zaznaczcie wspólnie, co myśli nastolatek słysząc wskazane poniżej zdania wypowiedziane przez rodziców. Do każdego zdania pasuje 1 interpretacja.</p>
         <Page92Widget2
             itemDescriptions={[
-                "zestaw elementów zapewniający sterowanie elementami, które mają na celu doprowadzenie ładunku do cylindra silnika tłokowego oraz usunięcie z niego wypracowanych gazów ",
-                "prądnica prądu przemiennego, często trójfazowa. Służy do zmiany energii mechanicznej w prąd przemienny.",
-                "element lub zestaw elementów elektronicznych służący do zamiany napięcia przemiennego na napięcie jednego znaku, które po dalszym odfiltrowaniu może być zmienione na napięcie stałe."
+                {nameId: 'n1', id: 'd1', text: "zestaw elementów zapewniający sterowanie elementami, które mają na celu doprowadzenie ładunku do cylindra silnika tłokowego oraz usunięcie z niego wypracowanych gazów "},
+                {nameId: 'n2', id: 'd2', text: "prądnica prądu przemiennego, często trójfazowa. Służy do zmiany energii mechanicznej w prąd przemienny."},
+                {nameId: 'n3', id: 'd3', text: "element lub zestaw elementów elektronicznych służący do zamiany napięcia przemiennego na napięcie jednego znaku, które po dalszym odfiltrowaniu może być zmienione na napięcie stałe."},
             ]}
             itemNames={[
-                "Układ rozrządu silnika tłokowego",
-                "Alternator  ",
-                "Prostownik"
+                {id: 'n1', text: "Układ rozrządu silnika tłokowego",},
+                {id: 'n2', text: "Alternator  ",},
+                {id: 'n3', text: "Prostownik"},
             ]}
+            onComplete={() => setShowNext(true)}
         />
         <div style={{height: '10em'}}></div>
     </PageLayout>
-        <button onClick={onNext}>Dalej</button>
+        {showNext && <button onClick={onNext}>Dalej</button>}
     </Box>
 }
