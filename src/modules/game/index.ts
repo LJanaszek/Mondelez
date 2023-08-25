b34997662caa07066d6f26896a4b7a0e465b9216
skirk20 <lukasjanaszek@gmail.com>
2023-08-25 10:56:26 +0200

    poprawione style

--- src/modules/game/index.ts
+++ src/modules/game/index.ts
@@ -3,7 +3,7 @@ import reducer from "./reducer";
 import { SCENARIO } from "./scenario";
 import { GameScenario } from "./types";
 
-const GAME_MODULE_LS_KEY = 'milka-game-v4'
+const GAME_MODULE_LS_KEY = 'milka'
 
 export type GameState = {
     // name: string,
