a43708f40112e2370e54d404ff0ffba18907eb0a
lukas <lukasjanaszek@gmail.com>
2023-08-30 22:13:22 +0200

    new

--- src/modules/game/index.ts
+++ src/modules/game/index.ts
@@ -3,7 +3,7 @@ import reducer from "./reducer";
 import { SCENARIO } from "./scenario";
 import { GameScenario } from "./types";
 
-const GAME_MODULE_LS_KEY = 'milkav2'
+const GAME_MODULE_LS_KEY = 'milkav3'
 
 export type GameState = {
     // name: string,
